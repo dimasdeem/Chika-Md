6e28815fccb0ac22b50aec4e699c54a7b21ea25d
dimasdeem <92859246+dimasdeem@users.noreply.github.com>
2022-06-13 12:53:21 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,25 +19,25 @@
   }
 
   // Setting Owner
-  global.owner = ['6281575886399','6281575886399','6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.premium = ['6281575886399'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
-  global.ownernomer = '6281575886399' //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.owner = ['6285156581890'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.premium = ['6285156581890'] //ubah jadi nomor mu, note tanda ' gausah di hapus!
+  global.ownernomer = '6285156581890' //ubah jadi nomor mu, note tanda ' gausah di hapus!
 
   // Set Nama Own & Bot
-  global.ownername = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
-  global.botname = 'Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.footer = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.ownername = 'HαckͥDeͣeͫm' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.botname = 'H4ckd33m-Bot' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+  global.footer = 'HαckͥDeͣeͫm' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Kebutuhan Button & Kontak
   //ubah aja kalau ada yang gapunya gsh di hps anggap aja credits :), note tanda ' gausah di hapus!
-  global.youtube = 'https://youtube.com/channel/UCtM-pDaaGVRe4BJ7w4qE4Bw'
-  global.mygc = 'https://chat.whatsapp.com/CRY23lVKmXB7sCMhHmcwVw'
-  global.myweb = 'https://api-riychdwayne.herokuapp.com'
+  global.youtube = ''
+  global.mygc = ''
+  global.myweb = 'https://menu-gabut.ml'
   global.region = 'indonesia'
 
   // Set Wm
-  global.packname = '© Chika-Md' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
-  global.author = 'Riy' //ubah jadi nama mu, note tanda ' gausah di hapus!
+  global.packname = '© H4ckd33m-Bot' //ubah jadi nama bot mu, note tanda ' gausah di hapus!
+  global.author = 'HαckͥDeͣeͫm' //ubah jadi nama mu, note tanda ' gausah di hapus!
 
   // Set Nama Session
   //gausah di apa² in!
@@ -45,7 +45,7 @@
 
   // Set Image
   //terserah mau ubah apa nggak, menurut gua jangan di ubah!
-  global.thumb = fs.readFileSync('./image/chika.jpg')
+  global.thumb = fs.readFileSync('./image/xintya.jpg')
   global.velochika = fs.readFileSync('./image/chika.mp4')
 
   // Set Image Welcome
